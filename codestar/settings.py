c6bb3b0e44c36ce0df0fa820f086ff7e6e89a754
merin86 <m_liljenroth@hotmail.com>
2024-03-14 12:40:47 +0000

    added comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-merin86-djangoblog-359g43kj105.ws-eu110.gitpod.io', '.herokuapp.com']
